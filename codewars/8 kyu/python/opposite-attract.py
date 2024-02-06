e37366c7975dc9c09ed1f12e0b0a4c856ebf56a3
Hannah L <106502799+ht-l1@users.noreply.github.com>
2024-02-06 16:32:45 -0500

    solve opposite-attract.py

---
+++ codewars/8 kyu/python/opposite-attract.py
@@ -0,0 +1,13 @@
+# better solution
+
+def lovefunc(flower1, flower2):
+    return flower1 % 2 != flower2 % 2
+
+
+# my solution
+
+def lovefunc(flower1, flower2):
+    if (flower1 % 2 == 0 and flower2 % 2 != 0) or (flower1 % 2 != 0 and flower2 % 2 == 0):
+        return True
+    else:  
+        return False
